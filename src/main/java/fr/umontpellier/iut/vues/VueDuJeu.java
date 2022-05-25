e8999afe49cd46acf9430b4fa4b9a193e0851d12
blanchetm <blanchet.mathieu30@gmail.com>
2022-05-25 16:50:07 +0200

    Fix global bt ONACTION fonctionne

--- src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
@@ -1,7 +1,13 @@
 package fr.umontpellier.iut.vues;
 
 import fr.umontpellier.iut.IJeu;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 /**
  * Cette classe correspond à la fenêtre principale de l'application.
@@ -12,15 +18,31 @@ import javafx.scene.layout.Pane;
  * (le joueur courant, les 5 cartes Wagons visibles, les destinations lors de l'étape d'initialisation de la partie, ...)
  * ainsi que les listeners à exécuter lorsque ces éléments changent
  */
-public class VueDuJeu extends Pane {
+public class VueDuJeu extends HBox {
 
     private IJeu jeu;
     private VuePlateau plateau;
+    private VueJoueurCourant vueJoueurCourant;
+
+    @FXML
+    private Button bt;
 
     public VueDuJeu(IJeu jeu) {
         this.jeu = jeu;
+        vueJoueurCourant = new VueJoueurCourant();
         plateau = new VuePlateau();
-        getChildren().add(plateau);
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/VueDuJeu.fxml"));
+            loader.setRoot(this);
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public IJeu getJeu() {
@@ -29,6 +51,11 @@ public class VueDuJeu extends Pane {
 
     public void creerBindings() {
 //        plateau.creerBindings();
+    }
+
+    @FXML
+    public void btOnAction(){
+        System.out.println("TEST FONCTIONNE");
     }
 
 }
